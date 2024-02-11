eb3bc8668d0e83b94e5660556b9fc55077f76cc1
Tristan Armitage <armitageth@gmail.com>
2024-02-11 14:56:16 -0500

    Added python and javascript to treesitter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 -- Configure treesitter
 require'nvim-treesitter.configs'.setup({
-    ensure_installed = { "lua", "rust" },
+    ensure_installed = { "javascript", "python", "lua", "rust" },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
