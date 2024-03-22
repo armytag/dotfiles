70f3c96a986641cb26259d6fd7babd2f2fdef5e3
Tristan Armitage <armitageth@gmail.com>
2024-03-22 11:36:02 -0400

    Starting to create snippets

--- nvim/lua/user/luasnip.lua
+++ nvim/lua/user/luasnip.lua
@@ -1,5 +1,30 @@
 local ls = require"luasnip"
--- local types = require"luasnip.util.types"
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
 
 ls.config.set_config {
     history = true,
@@ -29,4 +54,34 @@ vim.keymap.set("i", "<C-l>", function ()
 end)
 
 -- Reloading snippets file
-vim.keymap.set("n", "<leader>ss", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
+vim.keymap.set("n", "<leader>ss", "<cmd>source ~/.config/nvim/lua/user/luasnip.lua<CR>")
+
+-- Reset snippets
+
+-- Lua snippets
+ls.add_snippets("lua", {
+    s("req", fmt("local {} = require('{}')", { i(1, "default"), rep(1) })),
+})
+
+-- Javascript snippets
+ls.add_snippets("javascript", {
+    s( "doc", fmt(
+        [[
+        /**
+        * {}
+        * @param {{{}}} {}
+        */
+        function {}({}) {{
+            {}
+        }}
+        ]],
+        {
+            i(1, "Description"),
+            i(2, "type"),
+            i(3, "param"),
+            i(4, "function"),
+            i(5, "params"),
+            i(6, ""),
+        }
+    )),
+})
