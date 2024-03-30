7b418c642e95993da913ffc1d75e229cb5c731c2
Tristan Armitage <armitageth@gmail.com>
2024-03-30 10:41:10 -0400

    Switch to lazy.nvim package manager

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,21 +1,63 @@
 -- Plugins
-local luaPlug = function(file)
-    vim.cmd('luafile ~/.config/nvim/lua/user/plugs/'..file..'.lua')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.call('plug#begin', '~/.config/nvim/plugged')
+require("lazy").setup({
+    -- plenary
+    "nvim-lua/plenary.nvim",
+    -- treesitter
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    -- gitsigns
+    "lewis6991/gitsigns.nvim",
+    -- lspconfig
+    "neovim/nvim-lspconfig",
+    "onsails/lspkind.nvim",
+    -- telescope
+    "nvim-telescope/telescope.nvim",
+    -- nvim-cmp
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    -- LuaSnip
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    -- harpoon2
+    {"ThePrimeagen/harpoon", branch = "harpoon2"},
+    -- vim-godot
+    "habamax/vim-godot",
+    -- Colorschemes
+    "cryptomilk/nightcity.nvim",
+    "tiagovla/tokyodark.nvim",
+    "folke/tokyonight.nvim",
+    "vimpostor/vim-prism",
+    "sontungexpt/witch",
+    "glepnir/zephyr-nvim",
+})
+-- luaPlug('colorschemes')
+-- luaPlug('custom-theme')
+-- luaPlug('prism')
+-- luaPlug('nvim-cmp')
+-- luaPlug('lspconfig')
+-- luaPlug('lua-snip')
+-- luaPlug('vim-godot')
+-- luaPlug('treesitter')
+-- luaPlug('plenary')
+-- luaPlug('telescope')
+-- luaPlug('gitsigns')
+-- luaPlug('harpoon')
 
-luaPlug('colorschemes')
-luaPlug('custom-theme')
-luaPlug('prism')
-luaPlug('nvim-cmp')
-luaPlug('lspconfig')
-luaPlug('lua-snip')
-luaPlug('vim-godot')
-luaPlug('treesitter')
-luaPlug('plenary')
-luaPlug('telescope')
-luaPlug('gitsigns')
-luaPlug('harpoon')
-
-vim.call('plug#end')
